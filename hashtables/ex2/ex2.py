b524e7e49a327f409a75ffef57d33c1df9336681
popkdodge <popkdodge@gmail.com>
2020-10-02 13:45:46 -0400

    sprintc

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -10,5 +10,14 @@ def reconstruct_trip(tickets, length):
     YOUR CODE HERE
     """
     # Your code here
+    ticket_hm = {}
+    for ticket in tickets:
+        ticket_hm[ticket.source] = ticket.destination
 
+    route = []
+    for i in range(length):
+        if i == 0:
+            route.append(ticket_hm["NONE"])
+        else:
+            route.append(ticket_hm[route[i-1]])
     return route
